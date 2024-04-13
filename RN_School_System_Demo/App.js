b2ac45a4a4c63aa70a4d02b88d65b2aaf8b98a43
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-04-13 13:34:50 +0700

    Update profile frame

--- RN_School_System_Demo/App.js
+++ RN_School_System_Demo/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import NavBar from './components/NavBar';
+import Profile from './components/Profile';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <View>
       <NavBar />
+        </View>
+      <View style={styles.profileContainer}>
+        <Profile />
+      </View>
     </SafeAreaView>
   );
 }
@@ -13,5 +19,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  profileContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+    alignItems: 'center'
   }
+  
 });
--- RN_School_System_Demo/components/Profile.js
+++ RN_School_System_Demo/components/Profile.js
@@ -1,4 +1,26 @@
-import {View} from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'; 
+
 function Profile() {
-    
+  return (
+    <LinearGradient colors={['#FF0000', '#DF007A', '#BD00FF']} style={styles.profileContainer} >
+      <View>
+        
+      </View>
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    alignItems: 'center',
+    width: 370,
+    height: 130,
+    paddingHorizontal: 100,
+    borderRadius: 25
+  },
+
+});
+
+export default Profile;
