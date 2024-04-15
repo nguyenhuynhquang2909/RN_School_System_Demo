c013c6048da4fc67bf01bf4d4cdcf6da14cfb2d3
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-04-15 20:33:36 +0700

    Update homepage components

--- RN_School_System_Demo/components/Card.js
+++ RN_School_System_Demo/components/Card.js
@@ -1,4 +1,39 @@
-function Card() {
+import {View, Image, Text, StyleSheet} from "react-native";
+function Card({title, image, text}) {
+    return (
+    <View style={styles.frame}>
+        <View style={styles.supportFrame}>
+            <Text style={styles.text}>{text}</Text>
+            <Image source={image} style={styles.image}/>
+        </View>
+        <Text style={styles.text}>{title}</Text>
+    </View>
+    )
+}   
+const styles = StyleSheet.create({
+    frame: {
+        width: 101,
+        height: 76,
+        position: 'relative',
+        borderColor: 'black',
+        borderWidth: 2,
+        margin: 6,
+        padding: 3
+    },
+    supportFrame: {
+       flexDirection: 'row' ,
+       justifyContent: 'space-between',
+       paddingBottom: 10
+    },
+    image: {
+        width: 40,
+        height: 40,
+        resizeMode: 'cover'
+    },
+    text: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
 
-}
+});
 export default Card;
--- RN_School_System_Demo/screens/Home.js
+++ RN_School_System_Demo/screens/Home.js
@@ -1,20 +1,66 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, ScrollView} from 'react-native';
 import FunctionTitle from '../components/FunctionTitle';
+import Card from '../components/Card';
 function Home() {
     return (
     <View style={styles.container}>
          {/* School Updates */}
         <View>
             <FunctionTitle>School Updates</FunctionTitle>
-           
+            <ScrollView style={styles.cardContainer} horizontal={true}>
+                <View>
+                    <Card title="News" text={3} image={require('../assets/icons/newspaper-folded.png')}/>
+                </View>
+                <View>
+                    <Card title="Events" text={5} image={require('../assets/icons/event.png')}/>
+                </View>
+                <View>
+                    <Card title="Bulleting" text={11} image={require('../assets/icons/bulletin-board.png')}/>
+                </View>
+           </ScrollView>
         </View>
+
+        {/* Academic */}
+        <View>
+            <FunctionTitle>Academics</FunctionTitle>
+            <ScrollView style={styles.cardContainer} horizontal={true}>
+                <View>
+                    <Card title="Assignment" text={3} image={require('../assets/icons/newspaper-folded.png')}/>
+                </View>
+                <View>
+                    <Card title="Events" text={5} image={require('../assets/icons/event.png')}/>
+                </View>
+                <View>
+                    <Card title="Attendance" text={11} image={require('../assets/icons/bulletin-board.png')}/>
+                </View>
+           </ScrollView>
+        </View>
+
+        {/* Communication */}
+        <View>
+            <FunctionTitle>Academics</FunctionTitle>
+            <ScrollView style={styles.cardContainer} horizontal={true}>
+                <View>
+                    <Card title="Assignment" text={3} image={require('../assets/icons/newspaper-folded.png')}/>
+                </View>
+                <View>
+                    <Card title="Events" text={5} image={require('../assets/icons/event.png')}/>
+                </View>
+           </ScrollView>
+        </View>
+
     </View>
     )
 }
 const styles = StyleSheet.create({
     container: {
-      padding: 20
+      padding: 20,
+      margin: 20
+    },
+    cardContainer: {
+        flexDirection: 'row',
+        paddingVertical: 20
     }
 });
 export default Home;
